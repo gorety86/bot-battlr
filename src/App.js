ca7d797463a124492b7ad05bb7a591858f7e3c3b
gorety <wagutugorety@gmail.com>
2023-04-29 14:39:05 +0300

    App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import BotsPage from "./Components/BotsPage"
+import "./App.css"
+
+ function App() {
+  return (
+    <div className="App">
+      <BotsPage />
+    </div>
+  )
+}
+export default App;
