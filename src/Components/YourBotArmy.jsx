1beb567537fca9c19eb568d18ecc9770bc90506f
gorety <wagutugorety@gmail.com>
2023-04-29 15:04:31 +0300

    components

--- src/Components/YourBotArmy.jsx
+++ src/Components/YourBotArmy.jsx
@@ -6,9 +6,7 @@ function YourBotArmy(props) {
   const displayBots = props.bots.map(bot => {
     return <BotCard bot={bot} action={props.action} removeCard={props.removeCard} />
   })
-
-
-  return (
+     return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
